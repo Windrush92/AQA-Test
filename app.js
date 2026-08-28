577116575881b1d8635ed8540102dc2f47155e32
Windrush92 <gomez.pablo92@gmail.com>
2026-08-28 16:09:08 -0300

    Removida clave expuesta para evitar alertas de seguridad

--- app.js
+++ app.js
@@ -898,7 +898,7 @@ const DEFAULT_EMAIL_RECIPIENTS = ['ivanc@pwg.com.ar', 'sebastian.garcia@pwg.com.
 const RESEND_API_URL = 'https://api.resend.com/emails';
 
 function getResendApiKey() {
-    return localStorage.getItem('AQA_RESEND_API_KEY') || 're_grB92cZ1_Pw5mGsdvaFxSKX16rDnefGhH';
+    return localStorage.getItem('AQA_RESEND_API_KEY') || '';
 }
 
 function getSenderEmail() {
